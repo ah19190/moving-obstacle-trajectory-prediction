d7fc82beb0704714c437c0f71746a0e045a78b51
Brendan <100033072+ah19190@users.noreply.github.com>
2023-07-27 15:16:36 +0100

    Add fit2 method that does not use derivatives of x, y, z

--- src/_fit.py
+++ src/_fit.py
@@ -11,8 +11,81 @@ from pysindy.differentiation import FiniteDifference, SINDyDerivative
 from pysindy.optimizers import STLSQ
 import utils_graph
 
-def fit(u: np.ndarray,
+def fit1(u: np.ndarray,
         t: np.ndarray) -> Tuple[ps.SINDy, ps.SINDy, np.ndarray, np.ndarray]:
-    """Uses PySINDy to find the equation that best fits the data u.
+    """Uses PySINDy to find the equation that best fits the data u. Includes using derivatives of equations
     """
     optimizer = STLSQ(threshold=THRESHOLD, max_iter=MAX_ITERATIONS)
+    differentiation_method = FiniteDifference()
+    # pylint: disable=protected-access
+    udot = differentiation_method._differentiate(u, t)
+
+    # Get a model for the movement in x.
+    # logging.info("Model for x")
+    x = u[:, 0:1]
+    xdot = udot[:, 0:1]
+    datax = np.hstack((x, xdot))
+    modelx = ps.SINDy(optimizer=optimizer,
+                      differentiation_method=differentiation_method,
+                      feature_names=["x", "xdot"],
+                      discrete_time=False)
+    modelx.fit(datax, t=t, ensemble=True)
+    modelx.print()
+    # logging.info("coefficients: %s", modelx.coefficients().T)
+
+    # Get a model for the movement in y.
+    # logging.info("Model for y")
+    y = u[:, 1:2]
+    ydot = udot[:, 1:2]
+    datay = np.hstack((y, ydot))
+    modely = ps.SINDy(optimizer=optimizer,
+                      differentiation_method=differentiation_method,
+                      feature_names=["y", "ydot"],
+                      discrete_time=False)
+    modely.fit(datay, t=t, ensemble=True)
+    modely.print()
+    # logging.info("coefficients: %s", modely.coefficients().T)
+
+    # Get a model for the movement in z.
+    # logging.info("Model for z")
+    z = u[:, 2:3]
+    zdot = udot[:, 2:3]
+    dataz = np.hstack((z, zdot))
+    modelz = ps.SINDy(optimizer=optimizer,
+                      differentiation_method=differentiation_method,
+                      feature_names=["z", "zdot"],
+                      discrete_time=False)
+    modelz.fit(dataz, t=t, ensemble=True)
+    modelz.print()
+    # logging.info("coefficients: %s", modelz.coefficients().T)
+
+    return (modelx, modely, modelz, xdot, ydot, zdot)
+
+def fit2(u: np.ndarray,
+        t: np.ndarray) -> Tuple[ps.SINDy, np.ndarray, np.ndarray, np.ndarray]:
+    """Uses PySINDy to find the equation that best fits the data u. Does not use derivatives of equations. 
+    """
+    optimizer = STLSQ(threshold=THRESHOLD, max_iter=MAX_ITERATIONS)
+    differentiation_method = FiniteDifference()
+    
+    xdot = u[:, 0:1]
+    ydot = u[:, 1:2]
+    zdot = u[:, 2:3]
+
+    # Combine the data for all dimensions (x, y, and z)
+    data_all = np.hstack((u, xdot, ydot, zdot))
+    # Fit the model using pysindy
+    model_all = ps.SINDy(optimizer=optimizer,
+                        differentiation_method=differentiation_method,
+                        feature_names=["x", "xdot", "y", "ydot", "z", "zdot"],
+                        discrete_time=False)
+    model_all.fit(data_all, t=t, ensemble=True)
+    model_all.print()
+
+    return (model_all, xdot, ydot, zdot)
+
+
+def main() -> None:
+    # logging.info("Fitting.")
+
+    (modelx, modely, xdot, ydot) = fit1(u, t) # base case we are using fit 1 for now 
