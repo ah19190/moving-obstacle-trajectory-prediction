f326ee551a6fd62093a0fde3f344d6073b56c13a
Brendan <100033072+ah19190@users.noreply.github.com>
2023-07-27 13:17:18 +0100

    Add projectile function

--- src/generate_projectile_data.py
+++ src/generate_projectile_data.py
@@ -15,3 +15,17 @@ launch_angle = 30  # Launch angle in degrees
 # Time points for the trajectory
 t = np.arange(0, 2*v0/g, dt)
 
+# Function to calculate the projectile motion (remove z to follow the example)
+def projectile_motion(v0, theta_deg, t):
+    theta_rad = np.radians(theta_deg)
+    vx0 = v0 * np.cos(theta_rad)
+    vy0 = v0 * np.sin(theta_rad)
+    vz0 = v0 * np.cos(theta_rad)
+
+    x = x0 + vx0 * t
+    y = y0 + vy0 * t - 0.5 * g * t**2
+    z = z0 + vz0 * t - 0.5 * g * t**2
+
+    return np.array([x, y, z])
+    # return np.array([x, y])
+
